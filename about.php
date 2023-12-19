675fd57abe6fddf55c15c1d4077eab8efd2b957b
mayfors <154336502+mayfors@users.noreply.github.com>
2023-12-19 20:54:07 +0800

    Added project files

---
+++ about.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" contents="Connecting Worlds in PHP">
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <nav id="navigation">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="contents">
+        <p>Nothing to see here! Move along!</p>
+        </div>
+        <footer>
+            Contact me on Twitter: <a href="https://www.twitter.com/ElonMusk">@ElonMusk</a>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" contents="Connecting Worlds in PHP">
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <nav id="navigation">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="contents">
+        <p>Welcome to our page. Our aim is to connect people from different worlds through PHP. No creatures were harmed throughout the process.</p>
+        </div>
+        <footer>
+            Contact me on Twitter: <a href="https://www.twitter.com/ElonMusk">@ElonMusk</a>
+        </footer>
+    </body>
+</html>
